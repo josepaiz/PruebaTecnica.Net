4cd15d743948f5b3406c741aa1a1cccde5894770
josepaiz <101146291+josepaiz@users.noreply.github.com>
2023-08-05 18:01:14 -0600

    Inciso 4
    Se adjunta un programa de consola que puede ejecutar 3 métodos.
    GetData recibe una consulta SELECT y una cadena de conexión y retorna un dataset
    Sentences recibe una consulta si es SELECT devolvera la cantidad de lineas que devuelve la consulta, de ser INSERT, UPDATE O DELETE, devolverá cuantas líneas afecto, de igual manera SentenceConsole pero su diferencia que para efectos de pruebas devolvía la información mediante consola.

---
+++ Inciso4/Inciso4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Inciso4
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+               
+        metodos obj = new metodos();
+            obj.GetData("SELECT * FROM PRODUCTO" , "Data Source = DESK\\SQLEXPRESS; Initial Catalog = PruebaTecnica; user id = sa; password = 123");
+            obj.Sentences("DELETE FROM PROYECTO WHERE PROYECTO = 3", "Data Source = DESK\\SQLEXPRESS; Initial Catalog = PruebaTecnica; user id = sa; password = 123");
+            obj.SentencesConsole("UPDATE PROYECTO SET NOMBRE = 'PRUEBA' WHERE PROYECTO = 1", "Data Source = DESK\\SQLEXPRESS; Initial Catalog = PruebaTecnica; user id = sa; password = 123");
+
+        }
+    }
+}
---
+++ Inciso4/Inciso4/metodos.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Inciso4
+{
+
+    class metodos
+    {
+
+        public SqlConnection conexion = new SqlConnection();
+        
+        // PARA RETORNAR DATOS 
+        public DataSet GetData(string qry, string cadena ) 
+        {
+
+            try
+            {
+                conexion.ConnectionString = cadena;
+                SqlDataAdapter adapter;
+                DataSet ds = new DataSet();
+
+                string query = qry;
+                conexion.Open();
+                adapter = new SqlDataAdapter(query, conexion);
+                adapter.Fill(ds, "SELECT");
+                conexion.Close();
+                return ds;
+        }
+            catch(Exception e)
+            {
+                conexion.Close();
+
+            }
+            return null;
+         
+            
+
+        }
+
+        public string Sentences(string query , string cadena)
+        {
+            try
+            {
+                conexion.ConnectionString = cadena;
+                bool Select = query.ToUpper().Contains("SELECT");
+
+                if (Select == true)
+                {
+                    SqlDataAdapter adapter;
+                    DataSet ds = new DataSet();
+                    DataTable t = new DataTable();
+                    string query1 = query;
+                    conexion.Open();
+                    adapter = new SqlDataAdapter(query1, conexion);
+                    adapter.Fill(t);
+                    conexion.Close();
+
+                    int count = 0;
+                    DataRow[] rows;
+
+                    rows = t.Select();
+                    count = rows.Length;
+
+                    string response = ("Los registros de la consulta son: " + count.ToString());
+                    return response;
+                }
+                else
+                {
+                    int Cantidad;
+                    string qry = query;
+                    SqlCommand cmd = new SqlCommand(qry, conexion);
+                    conexion.Open();
+                    Cantidad = cmd.ExecuteNonQuery();
+                    conexion.Close();
+                    string response = ("Los registros afectados son: " + Cantidad + "");
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+
+        }
+
+
+        // VER DATOS EN CONSOLA 
+
+        public void SentencesConsole(string query, string cadena)
+        {
+            try
+            {
+                conexion.ConnectionString = cadena;
+                bool Select = query.ToUpper().Contains("SELECT");
+
+                if (Select == true)
+                {
+                    SqlDataAdapter adapter;
+                    DataSet ds = new DataSet();
+                    DataTable t = new DataTable();
+                    string query1 = query;
+                    conexion.Open();
+                    adapter = new SqlDataAdapter(query1, conexion);
+                    adapter.Fill(t);
+                    conexion.Close();
+
+                    int count = 0;
+                    DataRow[] rows;
+
+                    rows = t.Select();
+                    count = rows.Length;
+
+                    Console.WriteLine("Los registros de la consulta son: " + count.ToString());
+                   
+                }
+                else
+                {
+                    int Cantidad;
+                    string qry = query;
+                    SqlCommand cmd = new SqlCommand(qry, conexion);
+                    conexion.Open();
+                    Cantidad = cmd.ExecuteNonQuery();
+                    conexion.Close();
+                    Console.WriteLine("Los registros afectados son: " + Cantidad + "");
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine( e.Message);
+            }
+
+
+        }
+    }
+    }
+
+ 
